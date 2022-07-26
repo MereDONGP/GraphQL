61152d13b8ec357bb5c06a069152c4b799a133b3
Don <pullend15@gmail.com>
2022-07-26 08:51:12 -0400

    adding in the ability to change

--- frontend/src/components/Authors.jsx
+++ frontend/src/components/Authors.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 
 const Authors = ({people}) => {
 
+    const [ viewAuth, setViewAuth] = useState("block")
+    const [viewBooks, setViewBooks] = useState("none")
+    const [view, setView] = useState({authView: "block", authBook: "none"})
+
+/*     const changeView = () => {
+        if (viewAuth === "block"){
+            setViewAuth("none")
+            setViewBooks("block")
+        }
+        else if (viewBooks === "block"){
+            setViewBooks("none")
+            setViewAuth("block")
+        }
+    } */
+
     
     return(
         <>
+        <button onClick={() => setView({authView: "block", authBook: "none"})}>Authors</button>
+        <button onClick={() =>  setView({authView: "none", authBook: "block"})}>Books</button>
+        <div style={{"display":view.authView}}>
             {people.map((n,i) => 
-                <ul key={i}>
-                    <li>Name: {n.name}</li>
-                    <li>Born: {n.born}</li>
-                    <li>Book Count: {n.bookCount}</li>
-                </ul>
-            )}
+                    <ul key={i}>
+                        <li>Name: {n.name}</li>
+                        <li>Born: {n.born}</li>
+                        <li>Book Count: {n.bookCount}</li>
+                    </ul>
+                )}
+        </div>
+
+        <div style={{"display": view.authBook }}>
+            <h1>View Books</h1>
+        </div>
         </>
     )
 }
